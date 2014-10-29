5569636563bcd7d2c19a0a335a04ebcb2e30635f
Nikita Gazarov <nikita@raquo.org>
2014-10-29 21:09:06 +0000

    syntax panel toggle in JS, misc styles

---
+++ static/js/hnapp.js
@@ -0,0 +1,37 @@
+
+init();
+
+
+
+function init()
+{
+	// Enable toggle of syntax panel
+	document.getElementById('syntax_toggle').onclick = function()
+	{
+		if (elById('syntax').style.display == 'block')
+		{
+			elById('syntax').style.display = 'none';
+			window.location.hash = '';
+		}
+		else
+		{
+			elById('syntax').style.display = 'block';
+			window.location.hash = '#showsyntax';
+		}
+		return false;
+	}
+	
+	// Open syntax panel if requested
+	if (window.location.hash === '#showsyntax')
+	{
+		elById('syntax').style.display = 'block';
+	}
+}
+
+
+function elById(el_id)
+{
+	return document.getElementById(el_id);
+}
+
+
